0282447e0370fad28e1bc33a998a0a2054d25994
Zwiicker <hickzwicker@gmail.com>
2023-05-12 18:58:23 -0300

    ajustes

--- data/dataFake.ts
+++ data/dataFake.ts
@@ -2,18 +2,36 @@ export const dataFake = [
   {
     "id":"1",
     "title": "Guardiões da Galáxia Vol. 3 ",
-    "description": "Mais um live action chegando por aí! A Pequena Sereia ganha sua adaptação em 2023, com muita representatividade e a história mágica que a gente já conhece ♥ Sinopse: Remake live action do clássico desenho animado",
-    "estreia":"Data de Estreia: 04/04/2023",
+    "description": "Antes do fim de semana, a superprodução de super-heróis da Disney estava projetada para arrecadar entre US$130 milhões e US$140 milhões no exterior. Graças ao boca a boca positivo, as estimativas foram revisadas no domingo. Isso é uma ótima notícia para a Marvel, já que a temporada atual de filmes ficará cada vez mais competitiva com a estreia de Velozes e Furiosos 10 e A Pequena Sereia ainda neste mês.",
+    "estreia":"Data de Estréia: 04/04/2023",
+    "genero":"Gênero: Ação, Aventura, Super-herói",
     "videoUrl":"tqot0Ws2wFs",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "estreia":"Data de Estreia: 04/04/2023",
-    "videoUrl":"tqot0Ws2wFs",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "Velozes & Furiosos 10",
+    "description": "Velozes & Furiosos está chegando ao fim e 10º filme da franquia já tem data de lançamento confirmada pelo estúdio. Mais de duas décadas após estrear nas telonas, a franquia estrelada por Vin Diesel se encaminha para seus capítulos finais e o próximo filme já está próximo: com o título original de Fast X, Velozes & Furiosos 10 chega aos cinemas em 19 de maio de 2023. Esta será a penúltima produção da saga: Velozes & Furiosos 11.",
+    "genero":"Gênero: Ação, Carros",
+    "estreia":"Data de Estréia: 19/05/2023",
+    "videoUrl":"cWiK2hm4V28",
+  },
+
+  {
+    "id":"3",
+    "title": "Transformers: O Despertar das Feras",
+    "description": "Transformers: O Despertar das Feras‘ terá 117 minutos de duração sem incluir os créditos, ou seja, 1 hora e 57 minutos. Baseado na temporada “Beast Wars” da animação dos anos 1990, ‘Transformers: O Despertar das Feras‘ levará o público a uma aventura global com os Autobots em 1994. O filme apresentará a batalha existente na Terra entre Autobots e Decepticons aos Maximals, nova geração de Transformer.",
+    "genero":"Gênero: Ação, Aventura, Ficção científica",
+    "estreia":"Data de Estréia: 18/05/2023",
+    "videoUrl":"AjrAbcS7eyM",
+  },
+
+  {
+    "id":"4",
+    "title": " A Pequena Sereia",
+    "description": "A Disney divulgou o trailer oficial do live-action de “A Pequena Sereia” durante a cerimônia do Oscar. Halle Bailey e Melissa McCarthy, protagonista e vilã da produção, respectivamente, estavam no evento. O longa traz uma nova leitura da clássica animação musical. Ariel, a mais nova e a mais rebelde das filhas do Rei Tritão, sonha em conhecer o mundo além-mar e, ao visitar a superfície, apaixona-se pelo príncipe Eric. Confira o trailer:",
+    "genero":"Gênero: Fantasia, Infantil",
+    "estreia":"Data de Estréia: 25/05/2023",
+    "videoUrl":"_ma8MMmx72A",
   }
 ]
 
