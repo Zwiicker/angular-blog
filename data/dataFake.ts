285ae507a9e5c4e1189e842884309f8e197d2fc8
Zwiicker <hickzwicker@gmail.com>
2023-05-15 16:32:59 -0300

    mais ajustes na parte de dados.

--- data/dataFake.ts
+++ data/dataFake.ts
@@ -7,8 +7,17 @@ export const dataFake = [
     "genero":"Gênero: Ação, Aventura, Super-herói",
     "videoUrl":"tqot0Ws2wFs",
   },
+
   {
     "id":"2",
+    "title": "Homem-Aranha: Através do Aranhaverso",
+    "description": "Os fãs de Homem-Aranha já podem conferir o trailer de Homem-Aranha Através do Aranhaverso, lançado na última terça-feira (13) no canal da Sony Pictures no YouTube. A data de estreia do novo longa está prevista para o dia 1 de junho de 2023. Depois do sucesso do primeiro filme, Homem-Aranha no Aranhaverso (2019), o herói Miles Morales está de volta em uma nova aventura cheia de novas versões do personagem mais popular da Marvel.",
+    "genero":"Gênero: Comédia, Ação",
+    "estreia":"Data de Estréia: 01/06/2023",
+    "videoUrl":"LZBlXkDvhh4",
+  },
+  {
+    "id":"3",
     "title": "Velozes & Furiosos 10",
     "description": "Velozes & Furiosos está chegando ao fim e 10º filme da franquia já tem data de lançamento confirmada pelo estúdio. Mais de duas décadas após estrear nas telonas, a franquia estrelada por Vin Diesel se encaminha para seus capítulos finais e o próximo filme já está próximo: com o título original de Fast X, Velozes & Furiosos 10 chega aos cinemas em 19 de maio de 2023. Esta será a penúltima produção da saga: Velozes & Furiosos 11.",
     "genero":"Gênero: Ação, Carros",
@@ -17,21 +26,49 @@ export const dataFake = [
   },
 
   {
-    "id":"3",
+    "id":"4",
     "title": "Transformers: O Despertar das Feras",
-    "description": "Transformers: O Despertar das Feras‘ terá 117 minutos de duração sem incluir os créditos, ou seja, 1 hora e 57 minutos. Baseado na temporada “Beast Wars” da animação dos anos 1990, ‘Transformers: O Despertar das Feras‘ levará o público a uma aventura global com os Autobots em 1994. O filme apresentará a batalha existente na Terra entre Autobots e Decepticons aos Maximals, nova geração de Transformer.",
+    "description": "Transformers: O Despertar das Feras terá 117 minutos de duração sem incluir os créditos, ou seja, 1 hora e 57 minutos. Baseado na temporada “Beast Wars” da animação dos anos 1990, ‘Transformers: O Despertar das Feras‘ levará o público a uma aventura global com os Autobots em 1994. O filme apresentará a batalha existente na Terra entre Autobots e Decepticons aos Maximals, nova geração de Transformer.",
     "genero":"Gênero: Ação, Aventura, Ficção científica",
     "estreia":"Data de Estréia: 18/05/2023",
     "videoUrl":"AjrAbcS7eyM",
   },
 
   {
-    "id":"4",
+    "id":"5",
     "title": " A Pequena Sereia",
     "description": "A Disney divulgou o trailer oficial do live-action de “A Pequena Sereia” durante a cerimônia do Oscar. Halle Bailey e Melissa McCarthy, protagonista e vilã da produção, respectivamente, estavam no evento. O longa traz uma nova leitura da clássica animação musical. Ariel, a mais nova e a mais rebelde das filhas do Rei Tritão, sonha em conhecer o mundo além-mar e, ao visitar a superfície, apaixona-se pelo príncipe Eric. Confira o trailer:",
     "genero":"Gênero: Fantasia, Infantil",
     "estreia":"Data de Estréia: 25/05/2023",
     "videoUrl":"_ma8MMmx72A",
+  }
+  ,
+  {
+    "id":"6",
+    "title": " Shazam: Fúria dos Deuses",
+    "description": "O filme “Shazam! Fúria dos Deuses”, estrelado por Zachary Levi, já está nas plataformas digitais. A DC e a Warner Bros. fizeram o lançamento nesta terça (25/4) nos formatos de compra e aluguel. O longa-metragem ainda não está disponível no streaming.",
+    "genero":"Gênero: Ação, Aventura",
+    "estreia":"Data de Estréia: 01/06/2023",
+    "videoUrl":"Jl4DCMw6Frc",
+  }
+  ,
+
+  {
+    "id":"7",
+    "title": " Indiana Jones e o Chamado do Destino",
+    "description": "Indiana Jones está de volta! Durante a CCXP22, o quinto filme do arqueólogo mais amado da cultura pop teve seu primeiro trailer revelado. Além de trazer o retorno de Harrison Ford a um dos papéis mais icônicos de sua carreira depois de 14 anos, a prévia revelou também o título oficial da nova sequência: Indiana Jones e o Chamado do Destino. Assista ao trailer.",
+    "genero":"Gênero: Ação, Aventura",
+    "estreia":"Data de Estréia: 30/06/2023",
+    "videoUrl":"xkJETg95Zmc",
+  },
+
+  {
+    "id":"8",
+    "title": " Missão Impossível 7",
+    "description": "Missão: Impossível 7 chega aos cinemas dos EUA em 14 de julho de 2023, enquanto Missão: Impossível 8 fará sua estreia em 28 de junho de 2024.",
+    "genero":"Gênero: Ação, Aventura",
+    "estreia":"Data de Estréia: 13/07/2023",
+    "videoUrl":"W7cTpaDKfak",
   }
 ]
 
